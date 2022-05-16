2b25d5d0e194581926d3b86ec3f679e5c9d3b0fe
gren236 <gren236@gmail.com>
2022-05-16 21:13:26 +0300

    Add AsciiCharSequence task solution

---
+++ src/main/java/AsciiCharSequence/AsciiCharSequence.java
@@ -0,0 +1,37 @@
+package AsciiCharSequence;
+
+import java.util.Arrays;
+
+public class AsciiCharSequence implements CharSequence {
+    protected byte[] seq;
+
+    public AsciiCharSequence(byte[] seq) {
+        this.seq = seq.clone();
+    }
+
+    @Override
+    public int length() {
+        return seq.length;
+    }
+
+    @Override
+    public char charAt(int index) {
+        return (char)seq[index];
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        return new AsciiCharSequence(Arrays.copyOfRange(seq, start, end));
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+
+        for (byte val : seq) {
+            builder.append((char)val);
+        }
+
+        return builder.toString();
+    }
+}
---
+++ src/main/java/AsciiCharSequence/Main.java
@@ -0,0 +1,16 @@
+package AsciiCharSequence;
+
+public class Main {
+    public static void main(String[] args) {
+        byte[] example = {72, 101, 108, 108, 111, 33};
+        AsciiCharSequence answer = new AsciiCharSequence(example);
+        System.out.println("Sequence - " + answer.toString());//Hello!
+        System.out.println("Size - " + answer.length());//6
+        System.out.println("First symbol - " + answer.charAt(1));//e
+        System.out.println("Subsequence - " + answer.subSequence(1, 5));//ello
+        // check for private field encapsulation
+        System.out.println(answer.toString());//Hello!
+        example[0] = 74;
+        System.out.println(answer.toString());//Hello!
+    }
+}
